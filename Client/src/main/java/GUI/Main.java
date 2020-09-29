9aa8f6fb1724e3730db02b8be580e8403cdd67c1
Tristan Lenormand <tristan.lenormand@univ-nantes.fr>
2020-09-29 17:14:45 +0200

    modification of FX Application to make it work with scene builder in Client module, moving fx plugin in pom.xml from root to client.

--- Client/src/main/java/GUI/Main.java
+++ Client/src/main/java/GUI/Main.java
@@ -2,12 +2,37 @@ package GUI;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class Main  {
+import java.io.IOException;
 
-    public static void main(String[] args){
-        Application.launch(GUI.class, args);
-    }
+public class Main extends Application {
+    private Stage primaryStage;
+    private AnchorPane mainLayout;
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+                this.primaryStage=primaryStage;
+                this.primaryStage.setTitle("Undercover App");
+                showMainView();
+                primaryStage.show();
+
+        }
+
+        private void showMainView() throws IOException {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("MainView.fxml"));
+            mainLayout = loader.load();
+        }
+
+
+        public static void main(String[] args){
+            Application.launch(Main.class, args);
+        }
 
 
     }
