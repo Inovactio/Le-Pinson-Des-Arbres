300a6452e1ecb2a11890728d20ea2496741ba658
Josselin Enet <enet.josselin@gmail.com>
2020-09-29 14:52:58 +0200

    Added main Server methods

--- Server/src/main/java/IServerGame.java
+++ Server/src/main/java/IServerGame.java
@@ -2,4 +2,7 @@ import java.rmi.Remote;
 
 public interface IServerGame  extends Remote{
 
+    public void write(String word);
+
+    public void vote(String player);
 }
--- Server/src/main/java/ServerGame.java
+++ Server/src/main/java/ServerGame.java
@@ -1,26 +1,34 @@
-import com.sun.security.ntlm.Server;
-
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class ServerGame extends UnicastRemoteObject implements IServerGame{
+public class ServerGame extends UnicastRemoteObject implements IServerGame {
 
-    public ServerGame() throws RemoteException{
+    public ServerGame() throws RemoteException {
 
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
             LocateRegistry.createRegistry(8090);
             ServerGame serverGame = new ServerGame();
-            Naming.bind("",serverGame);
-        }catch (Exception e){
+            Naming.bind("localhost", serverGame);
+        } catch (Exception e) {
             System.out.println("Erreur Server Main");
         }
+    }
 
+    public void vote(String player) {
+        // TODO Auto-generated method stub
 
     }
+
+    public void write(String word) {
+        // TODO Auto-generated method stub
+
+    }
+
+    
 
 }
