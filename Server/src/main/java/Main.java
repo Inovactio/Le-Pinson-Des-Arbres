e9f4501e0df54dd54771faa37db2d2c23606f7a0
Kevin ROY <E165593S@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2020-09-29 16:18:13 +0200

    Add an initialization for the database

---
+++ Server/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.sql.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        try {
+            /*Création du serveur*/
+            LocateRegistry.createRegistry(8090);
+            ServerGame serverGame = new ServerGame();
+            Naming.bind("//localhost:8090", serverGame);
+
+
+
+
+
+
+
+        } catch (Exception e) {
+            System.out.println("Erreur lors de la création du serveur de jeu.");
+        }
+    }
+}
