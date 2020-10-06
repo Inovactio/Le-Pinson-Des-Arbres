6f14e9b7d81ca3832cb6fa4e536f3ad9afd1f954
Josselin Enet <enet.josselin@gmail.com>
2020-10-06 10:27:07 +0200

    Fixed Server launch

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -1,29 +1,28 @@
-
-
 import jsonparser.JsonParser;
 
+import java.net.InetAddress;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
 
 public class Main {
 
-
-
     public static void main(String[] args) {
 
         try {
             /*Création du serveur*/
-            LocateRegistry.createRegistry(8090);
+            final String url = "localhost";
+            final int port = 8090;
+            LocateRegistry.createRegistry(port);
             ServerGame serverGame = new ServerGame();
-            Naming.bind("//localhost:8090", serverGame);
+            
+            Naming.rebind("//"+url+":"+port+"/undercover", serverGame);
 
             JsonParser jsonParser = new JsonParser("Server/src/main/resources/words.json");
 
-
-
         } catch (Exception e) {
             System.out.println("Erreur lors de la création du serveur de jeu.");
+            e.printStackTrace();
         }
     }
 }
