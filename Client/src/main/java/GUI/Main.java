d38248b0d416f542430d125619bdc2639e6ef55e
Tristan Lenormand <tristan.lenormand@univ-nantes.fr>
2020-10-22 18:23:33 +0200

    godsjg

---
+++ Client/src/main/java/GUI/Main.java
@@ -0,0 +1,26 @@
+package GUI;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+        public static void main(String[] args){
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("/Undercover.fxml"));
+            primaryStage.setTitle("Undercover");
+            primaryStage.setScene(new Scene(root,1080,720));
+            primaryStage.show();
+
+        }
+
+    }
+
+
+
