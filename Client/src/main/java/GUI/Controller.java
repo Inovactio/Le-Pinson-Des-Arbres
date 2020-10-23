b4a7a64d0d4a2e1b45fdd0854c171679fd51bb8b
Tristan Lenormand <tristan.lenormand@univ-nantes.fr>
2020-10-23 17:58:11 +0200

    keep implementing the lobby with fx

--- Client/src/main/java/GUI/Controller.java
+++ Client/src/main/java/GUI/Controller.java
@@ -7,12 +7,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+
 
 
 public class Controller {
 
     @FXML
     private Button createLobby,joinLobby;
+    private TextField labelmots, labeltours, tempstours,nbimposteurs;
+    private int valuemots = 12;
+
+
 
     @FXML
     private void createLobbyButtonAction(ActionEvent event) throws Exception {
@@ -24,6 +30,26 @@ public class Controller {
         Scene scene = new Scene(root,1080,720);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    private void incrementNumberOfWords(ActionEvent event) throws Exception {
+        System.out.println(Integer.toString(valuemots));
+        labelmots.setText(Integer.toString(valuemots));
+        System.out.println(labelmots.getText());
+        int value = Integer.parseInt(labelmots.getText())+1;
+        System.out.println(value);
+        labelmots.setText(Integer.toString(value));
+        System.out.println(labelmots.getText());
+
+
+    }
+
+    @FXML
+    private void test1(ActionEvent event) throws Exception {
+        labelmots.setText(Integer.toString(12));
+        System.out.println(labelmots.getText()==null);
+
     }
 
 }
